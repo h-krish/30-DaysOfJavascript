7a0c28801c274cc0b3add0f92d3e90f0ef2c248a
hfactor <hkrish@pm.me>
2022-09-02 11:06:40 +0530

    First day challenge completed

---
+++ Day1/variable.js
@@ -0,0 +1,13 @@
+var myName="Harikrishnan M M"
+let myAge=29
+var isMarried=false
+var myProfession
+console.log(myName,myAge)// Remember printing multiple variables using a comma will always produce a space b/w them
+console.log(typeof isMarried)// Remember typeof is an operator and not a method
+console.log(typeof myProfession)
+var x=5,y=6//Assigning multiple variables in a single line
+console.log(x+y)
+console.log(8/3)
+console.log(8%3)
+var z=8/3
+console.log(typeof z)
